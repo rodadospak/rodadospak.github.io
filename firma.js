f5baab83a7c38f467af0545f3534c49b84a3408f
rodadospak <rodados.parque.arauco@gmail.com>
2024-05-27 17:59:25 -0400

    Update firma.js

--- firma.js
+++ firma.js
@@ -1,6 +1,5 @@
 var canvas = document.getElementById('canvas');
 var signaturePad = new SignaturePad(canvas, {
-    // Habilitar soporte táctil
     onBegin: function (event) {
         if (event.type === "mousedown") {
             return;
@@ -19,5 +18,16 @@ function guardarFirma() {
         enlace.href = imagen;
         enlace.download = "firma.png";
         enlace.click();
+
+        // Guardar la imagen en el almacenamiento local del navegador
+        localStorage.setItem("ultimaFirma", imagen);
     }
 }
+
+// Recuperar la última firma guardada al cargar la página
+window.onload = function() {
+    var ultimaFirma = localStorage.getItem("ultimaFirma");
+    if (ultimaFirma) {
+        // Puedes hacer algo con la última firma aquí, si es necesario
+    }
+};
