cd040875053ad2dae6a60a056fe251b7d137114e
rodadospak <rodados.parque.arauco@gmail.com>
2024-07-06 23:42:23 -0400

    Update firma.js

--- firma.js
+++ firma.js
@@ -1,4 +1,16 @@
 var canvas = document.getElementById('canvas');
+
+// Asegúrate de que el lienzo tenga la misma relación de aspecto y resolución del dispositivo
+function resizeCanvas() {
+    var ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
+
 var signaturePad = new SignaturePad(canvas);
 
 function guardarFirma() {
